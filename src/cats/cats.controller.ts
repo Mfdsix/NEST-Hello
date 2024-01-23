760b41ddc35b40678b0aa315f541fd980e183808
mfdsix <mfdsix.1nd0@gmail.com>
2024-01-23 15:19:43 +0700

    add crud cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -12,14 +13,22 @@ export class CatsController {
         return 'Test GET Cat using Async';
     }
 
+    @Post()
+    @HttpCode(201)
+    create(@Body() body: CreateCatDto): string {
+        return 'Test POST Cat';
+    }
+
     @Get(':id')
     findOne(@Param() params: any): string {
         return 'Test GET Cat with ID ' + params.id;
     }
 
-    @Post()
-    @HttpCode(201)
-    create(): string {
-        return 'Test POST Cat';
-    }
+    @Put(':id')
+    @HttpCode(204)
+    update(@Param('id') id: string, @Body() body: CreateCatDto) {}
+
+    @Delete(':id')
+    @HttpCode(204)
+    delete(@Param('id') id: string) {}
 }
