0e214a6573010a939dd8a503133a306397f8356b
mfdsix <mfdsix.1nd0@gmail.com>
2024-01-23 15:12:25 +0700

    define basic cat routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
@@ -7,4 +7,19 @@ export class CatsController {
         return 'Test GET Cat';
     }
 
+    @Get('async')
+    async findAllAsync(): Promise<string> {
+        return 'Test GET Cat using Async';
+    }
+
+    @Get(':id')
+    findOne(@Param() params: any): string {
+        return 'Test GET Cat with ID ' + params.id;
+    }
+
+    @Post()
+    @HttpCode(201)
+    create(): string {
+        return 'Test POST Cat';
+    }
 }
